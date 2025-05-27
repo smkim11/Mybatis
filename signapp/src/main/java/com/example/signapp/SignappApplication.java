b194c38a90718e285520c5d52f294e8e55f65660
kim <smkim000721@naver.com>
2025-05-27 12:41:29 +0900

    C드라이브에서 이미지가져오기 위해 설정변경

--- signapp/src/main/java/com/example/signapp/SignappApplication.java
+++ signapp/src/main/java/com/example/signapp/SignappApplication.java
@@ -2,12 +2,18 @@ package com.example.signapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SignappApplication {
+public class SignappApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SignappApplication.class, args);
 	}
-
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/sign_img/**").addResourceLocations("file:///C:/sign_img");
+	}
 }
