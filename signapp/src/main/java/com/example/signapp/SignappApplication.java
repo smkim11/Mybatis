ead35df067bf79726ff2147a9b072ffc84106406
kim <smkim000721@naver.com>
2025-05-28 12:01:41 +0900

    @ServletComponentScan 추가하여 필터 적용

--- signapp/src/main/java/com/example/signapp/SignappApplication.java
+++ signapp/src/main/java/com/example/signapp/SignappApplication.java
@@ -2,10 +2,12 @@ package com.example.signapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@ServletComponentScan // 필터
 public class SignappApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
