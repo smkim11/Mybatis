466551ef9055e36839d8ea18c0eb1a24a88b68ba
kim <smkim000721@naver.com>
2025-05-28 12:05:15 +0900

    문서에 결제한 사람이 없고, 작성자일 경우에만 수정,삭제 가능

--- signapp/src/main/java/com/example/signapp/controller/SignController.java
+++ signapp/src/main/java/com/example/signapp/controller/SignController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.signapp.dto.Document;
 import com.example.signapp.dto.Employee;
@@ -84,20 +85,61 @@ public class SignController {
 	
 	// 문서 수정 (결제 안했을때 작성자만 수정)
 	@GetMapping("/updateDoc")
-	public String updateDoc(Model model, int documentNo) {
+	public String updateDoc(Model model, @RequestParam int documentNo
+							,RedirectAttributes redirectAttributes, HttpSession session) {
+		// 로그인한 사용자 정보
+		Employee loginUser = (Employee) session.getAttribute("loginInfo");
+		
+		// 결제한 사람이 없고, 작성자일 경우에만 수정
 		if(signService.documentOne(documentNo).getSignLevel2() == null
-		&& signService.documentOne(documentNo).getSignLevel3() == null) {
+		&& signService.documentOne(documentNo).getSignLevel3() == null
+		&& loginUser.getName().equals(signService.documentOne(documentNo).getWriter())) {
 			model.addAttribute("doc", signService.documentOne(documentNo));
 			return "updateDoc";
 		}
 		
+		// 오류메세지 
+		redirectAttributes.addFlashAttribute("msg","수정 불가");
 		return "redirect:/docOne?documentNo="+documentNo;
 	}
 	
 	@PostMapping("/updateDoc")
 	public String updateDoc(Document document) {
+		signService.updateDocument(document);
 		return "redirect:/docOne?documentNo="+document.getDocumentNo();
 	}
 	
-	// 문서 삭제 (결제 안했을때)
+	// 문서 삭제
+	@GetMapping("/deleteDoc")
+	public String deleteDoc(Model model, @RequestParam int documentNo, @RequestParam String writer
+							,RedirectAttributes redirectAttributes, HttpSession session) {
+		// 로그인한 사용자 정보
+		Employee loginUser = (Employee) session.getAttribute("loginInfo");
+		
+		// 결제한 사람이 없고, 작성자일 경우에만 삭제
+		if(signService.documentOne(documentNo).getSignLevel2() == null
+		&& signService.documentOne(documentNo).getSignLevel3() == null
+		&& loginUser.getName().equals(signService.documentOne(documentNo).getWriter())) {
+			model.addAttribute("writer",writer);
+			model.addAttribute("documentNo",documentNo);
+			return "deleteDoc";
+		}
+		
+		// 오류메세지 
+		redirectAttributes.addFlashAttribute("msg","삭제 불가");
+		return "redirect:/docOne?documentNo="+documentNo;
+	}
+	
+	@PostMapping("/deleteDoc")
+	public String deleteDoc(@RequestParam int documentNo, @RequestParam String pw
+							,@RequestParam String writer, RedirectAttributes redirectAttributes) {
+		if(pw.equals(signService.searchPw(writer))) {
+			signService.deleteDocument(documentNo);
+			return "redirect:/docView";
+		}
+		
+		// 오류메세지 
+		redirectAttributes.addFlashAttribute("msg","삭제 오류");
+		return "redirect:/docOne?documentNo="+documentNo;
+	}
 }
