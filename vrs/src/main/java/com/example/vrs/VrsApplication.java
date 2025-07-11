7dca860ee2b4af5d5a539ed5530807d3c312b366
kim <smkim000721@naver.com>
2025-07-11 16:18:32 +0900

    OAuth2 인증후 식당예약 시스템

---
+++ vrs/src/main/java/com/example/vrs/VrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.vrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrsApplication.class, args);
+	}
+
+}
