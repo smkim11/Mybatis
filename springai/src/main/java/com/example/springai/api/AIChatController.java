53655791d424b43594d63be9dfde575b8e02701b
kim <smkim000721@naver.com>
2025-07-16 12:11:58 +0900

    open ai 활용 연습

---
+++ springai/src/main/java/com/example/springai/api/AIChatController.java
@@ -0,0 +1,27 @@
+package com.example.springai.api;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springai.service.AIChatService;
+
+import jakarta.servlet.http.HttpSession;
+
+@RestController
+public class AIChatController {
+	private final AIChatService aIChatService;
+	public AIChatController(AIChatService aIChatService) {
+		this.aIChatService = aIChatService;
+	}
+	
+	@PostMapping("/chat")
+	public String chat(@RequestBody Map<String,String> body, HttpSession session) { // session 속성안에 message 리스트를 만들어 이전대화를 누적
+		// {"":"","":""} JSON 문자열 -> 자바 DTO객체 (@RequestBody가 변경해준다)
+		String userMsg = body.get("userMsg");
+		String aiReply = aIChatService.generate(userMsg, session);
+		return aiReply;
+	}
+}
---
+++ springai/src/main/java/com/example/springai/service/AIChatService.java
@@ -0,0 +1,70 @@
+package com.example.springai.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.stereotype.Service;
+
+import jakarta.servlet.http.HttpSession;
+
+@Service
+public class AIChatService {
+	// 대화형 AI 모델
+	private OpenAiChatModel openAiChatModel;
+	
+	public AIChatService(OpenAiChatModel openAiChatModel) {
+		this.openAiChatModel = openAiChatModel;
+	}
+	
+	// OpenAI서버와 통신할 메소드 선언
+	// param String userMsg : 사용자 문자열 메세지
+	// return String : 챗서버의 응답문자열
+	public String generate(String userMsg, HttpSession session) {
+		
+		List<Message> messageList = (List<Message>)session.getAttribute("chatHistory");
+		
+		if(messageList == null) {
+			messageList = new ArrayList<>(); // 이전 이력없이 처음하는 대화이면
+		}
+		
+		
+		// 챗봇의 답변스타일을 지정
+		SystemMessage systemMessage = new SystemMessage("너는 한국어로 반말로만 답변하는 스타일의 AI 챗봇이다.");
+		// 사용자 메세지
+		UserMessage userMessage = new UserMessage(userMsg);
+		
+		// List<Object> messageList = List.of(userMessage,systemMessage);
+	    messageList.add(userMessage);
+	    messageList.add(systemMessage);
+	    
+		// 옵션
+	    OpenAiChatOptions options = OpenAiChatOptions.builder()
+	            .model("gpt-4.1-mini") // 사용하고자 하는 OpenAI 모델(버전)의 이름을 지정
+	            .temperature(0.7) // 창의성(무작위성) 정도를 설정(0.0 ~ 2.0)값으로 보통 0~1 사이 사용
+	            .build();
+	    
+	    // OpenAI서버에 전달되는 모델 객체(매개값)
+	    Prompt prompt = new Prompt(messageList,options);
+		
+	    // openAiChatModel빈을 통해 Prompt를 OpenAI서버에 전달
+	    ChatResponse res = this.openAiChatModel.call(prompt);
+	    String aiReply = res.getResult().getOutput().getText();
+	    
+	    // ai가 응답한 응답들도 messageList에 누적
+	    AssistantMessage assistantMessage = new AssistantMessage(aiReply);
+	    messageList.add(assistantMessage);
+	    
+	    // messageList 변경된 내용을 session의 messageList속성에도 반영
+	    session.setAttribute("chatHistory", assistantMessage); // 이전 session chatHistory 속성값을 덮어쓰기
+	    
+		return aiReply;
+	}
+}
